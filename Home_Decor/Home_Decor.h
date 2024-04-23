e50650c6c1087c6cececb5239197a01ecf01663a
Tasneem357Mohamed <152304186+Tasneem357Mohamed@users.noreply.github.com>
2024-04-23 15:52:42 +0200

    Update Home_Decor.h

--- Home_Decor/Home_Decor.h
+++ Home_Decor/Home_Decor.h
@@ -3,31 +3,18 @@
 using namespace std;
 class Store
 {
+private:
     int Store_ID;
     string Store_Name;
     //list of products.
 public:
-    Store()
-    {
-        Store_ID = 0;
-        Store_Name = "nothing";
-    }
-    Store(int ID , string Name)
-    {
-        this->Store_ID = ID;
-        this->Store_Name = Name;
-    }
-    void set_Store_ID(int ID)
-    {
-        this->Store_ID = ID;
-    }
-    void set_Store_Name(string Name)
-    {
-        this->Store_Name = Name;
-    }
+    Store();
+    Store(int ID , string Name);
+    void set_Store_ID(int ID);
+    void set_Store_Name(string Name);
 };
 class Admin
 {
-    string UserName;
-    string Password;
+    string Admin_UserName;
+    string Admin_Password;
 };
