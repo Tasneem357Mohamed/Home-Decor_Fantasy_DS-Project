48f6b5652da02caa3c356bbd032d03096b658f05
admin <tasneem357mohamed@gmail.com>
2024-04-23 13:39:48 +0200

    "change"

--- Home_Decor.h
+++ Home_Decor.h
@@ -3,11 +3,31 @@
 using namespace std;
 class Store
 {
-    int ID;
-    string name;
+    int Store_ID;
+    string Store_Name;
     //list of products.
+public:
+    Store()
+    {
+        Store_ID = 0;
+        Store_Name = "nothing";
+    }
+    Store(int ID , string Name)
+    {
+        this->Store_ID = ID;
+        this->Store_Name = Name;
+    }
+    void set_Store_ID(int ID)
+    {
+        this->Store_ID = ID;
+    }
+    void set_Store_Name(string Name)
+    {
+        this->Store_Name = Name;
+    }
 };
 class Admin
 {
-
+    string UserName;
+    string Password;
 };
