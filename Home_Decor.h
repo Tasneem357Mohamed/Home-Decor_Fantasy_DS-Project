b8e71c282b8e914d0f4e5826600c861ab43f7038
admin <tasneem357mohamed@gmail.com>
2024-04-23 15:48:03 +0200

    "change Admin"

--- Home_Decor.h
+++ Home_Decor.h
@@ -7,24 +7,10 @@ class Store
     string Store_Name;
     //list of products.
 public:
-    Store()
-    {
-        Store_ID = 0;
-        Store_Name = "nothing";
-    }
-    Store(int ID , string Name)
-    {
-        this->Store_ID = ID;
-        this->Store_Name = Name;
-    }
-    void set_Store_ID(int ID)
-    {
-        this->Store_ID = ID;
-    }
-    void set_Store_Name(string Name)
-    {
-        this->Store_Name = Name;
-    }
+    Store();
+    Store(int ID , string Name);
+    void set_Store_ID(int ID);
+    void set_Store_Name(string Name);
 };
 class Admin
 {
