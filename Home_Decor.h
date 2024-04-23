d761a6834b10f5739112b91a7d374b3d9b5385f0
Tasneem357Mohamed <152304186+Tasneem357Mohamed@users.noreply.github.com>
2024-04-23 22:17:12 +0200

    Add files via upload

---
+++ Home_Decor.h
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+#include <map>
+using namespace std;
+class Store
+{
+private:
+    int Store_ID;
+    string Store_Name;
+    //list of products.
+public:
+    Store();
+    Store(int ID , string Name);
+    void set_Store_ID(int ID);
+    void set_Store_Name(string Name);
+};
+class Admin
+{
+    string Admin_UserName;
+    string Admin_Password;
+};
+class User {
+private:
+    string username;
+    string password;
+    string email;
+
+public:
+    User(string& username,  string& password,  string& email);
+    string getUsername();
+    void setUsername( string& newUsername);
+    string getPassword();
+    void setPassword( string& newPassword);
+    string getEmail();
+    void setEmail( string& newEmail);
+};
+
+class UserManager {
+private:
+    map<string, User> users;
+    string currentUser;
+
+public:
+    UserManager();
+    void signUpUser( string& username,  string& password,  string& email);
+    void signInUser( string& username,  string& password);
+    void signOutUser();
+    void editUser();
+    bool isUserSignedIn()const;
+    void choise();
+};
